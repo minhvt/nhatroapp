4857636798a44dbe1bb068f39b0a8bdba3c0c5ae
Victor Minh <minhvt.hanoi@gmail.com>
2023-07-28 16:44:21 +0700

    code giao diện các components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import DanhSachPhong from './components/DanhSachPhong';
+import DanhSachHoaDon from './components/DanhSachHoaDon';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Ứng dụng quản lý nhà trọ - &copy; Trandu 2023
       </header>
+      <div className="box-container">        
+        <Link to="/">Trang Chủ</Link>
+        <Link to="/phong-ds">Phòng</Link>
+        <Link to="/hoadon-ds">Hóa Đơn</Link>
+      </div>
+
+      <div className="box-container">
+        <Routes>
+          <Route path='/' Component={Login}></Route>
+          <Route path='/phong-ds' Component={DanhSachPhong}></Route>
+          <Route path='/hoadon-ds' Component={DanhSachHoaDon}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
